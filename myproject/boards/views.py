5e58f9b6159341599577909c4397da2bbfd42d1d
abdul <s201459620@kfupm.edu.sa>
2019-10-22 20:50:57 +0300

    Advanced urls and test

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Board
 # Create your views here.
 
@@ -7,3 +7,7 @@ def home(request):
     boards = Board.objects.all()
 
     return render(request, 'home.html', {'boards': boards})
+
+def board_topics(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    return render(request, 'topics.html', {'board': board})
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -14,14 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from django.conf.urls import url
 
 from boards import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
     url(r'^admin/', admin.site.urls),
-    url(r'^homepage/$', views.home, name='home')
+    url(r'^homepage/$', views.home, name='home'),
+    url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+
 ]
